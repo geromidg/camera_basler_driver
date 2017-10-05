0684e1c9ac86f643f3971ed2592b4e0434bdd3b8
Dimitris Geromichalos <geromidg@gmail.com>
2017-10-05 11:47:20 +0200

    Create unit test for camera's grab functionality

---
+++ test/test_CameraBasler.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+#include <camera_basler/CamGigEBasler.hpp>
+
+namespace camera
+{
+
+    BOOST_AUTO_TEST_CASE(grab_one_image)
+    {
+        CamGigEBasler camGigEBasler;
+
+        camGigEBasler.grab();
+    }
+
+}  // namespace camera
+
